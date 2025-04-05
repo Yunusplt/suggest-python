d144822589b2ffc4a9539199e4edfe305d21d8df
yunus <yunus.polat@simdatix.com>
2025-04-05 10:29:34 +0200

    create suggest model and add top-1000-product.json file

--- suggest/models.py
+++ suggest/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Suggestion(models.Model):
+    product_name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product_name
